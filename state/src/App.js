d3d5d5229d904741349a150fa74f742617e795a5
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-20 14:03:05 -0400

    count and color persist through toggling, because they are stored in central App component, which never leaves the page

--- state/src/App.js
+++ state/src/App.js
@@ -1,10 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+
 import './App.css';
+// import ColorChanger from './components/ColorChanger';
+// import Counter from './components/Counter'
 
 function App() {
-  return (
+  const [color, setColor] = useState('blue')
+  const [number, setNumber] = useState(0)
+  const [showing, setShowing] = useState('color-changer')
+
+  return (    
     <div className="container">
-      
+      <nav>
+        <span onClick={() => { setShowing('color-changer')}}>ColorChanger</span>
+        {' '}
+        <span onClick={() => { setShowing('counter') }}>Counter</span>
+      </nav>
+
+      { showing === 'color-changer' ? 
+        <div>
+          <div className={`box ${color}`} />
+          <div className="buttons-container">
+            <button onClick={() => { setColor('red') }}>Red</button>
+            <button onClick={() => { setColor('blue') }}>Blue</button>
+            <button onClick={() => { setColor('green') }}>Green</button>
+          </div>
+        </div>
+        : null
+      }
+
+      {showing === 'counter' ? 
+        <div>
+          <div>{number}</div>
+          <button onClick={() => { setNumber(number + 1) }}>+</button>
+          <button onClick={() => { setNumber(number - 1) }}>-</button>
+        </div>
+        : null
+      }
     </div>
   );
 }
